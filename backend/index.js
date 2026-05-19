71b14b7df403855de4743e7ae39224cd51e3476c
jadeT29 <jadetura28@gmail.com>
2026-05-19 21:30:13 +0800

    backend: feat: Enable CORS

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const prisma = require('./db');
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // Health check route
